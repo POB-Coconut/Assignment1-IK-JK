bcc47fa3c189611884eb31e03375bfa3adc37515
jaekyung lee <jaekyunglee@jaekyungui-MacBookPro.local>
2021-07-27 17:46:20 +0900

    #3

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from "styled-components";
+const GlobalStyles = createGlobalStyle`
+    *{
+              box-sizing:border-box;
+    }
+`;
+export default GlobalStyles;
