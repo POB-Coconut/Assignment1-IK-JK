10ff03c621b5dd1028e6ab5e2bed98894f528193
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-07-27 20:21:46 +0900

    feat: [#3]CardListUI 구성
    - API Data로 구성

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,58 +1,60 @@
-import React from "react";
-import styled from "styled-components";
-const Card = () => {
-	return (
-		<CardContainer>
-			<CardCommentId>
-				<Title>Comment Id</Title>
-				<Contents>sosdkjflksdhfklhl</Contents>
-			</CardCommentId>
-			<CardEmail>
-				<Title>Email</Title>
-				<Contents>slkdf@abfkshdfl.co.kr</Contents>
-			</CardEmail>
-			<CardComment>
-				<Title>Comment</Title>
-				<Contents>klsdhflkdfhglkh</Contents>
-			</CardComment>
-		</CardContainer>
-	);
+import React from 'react';
+import styled from 'styled-components';
+const Card = ({ ...card }) => {
+  const { name, email, body } = card.card;
+  return (
+    <CardContainer>
+      <CardCommentId>
+        <Title>Comment Id</Title>
+        <Contents>{name}</Contents>
+      </CardCommentId>
+      <CardEmail>
+        <Title>Email</Title>
+        <Contents>{email}</Contents>
+      </CardEmail>
+      <CardComment>
+        <Title>Comment</Title>
+        <Contents>{body}</Contents>
+      </CardComment>
+    </CardContainer>
+  );
 };
 
 export default Card;
 const CardContainer = styled.div`
-	width: 500px;
-	height: auto;
-	box-sizing: border-box;
-	background-color: #f8f9fa;
-	color: #212529;
-	border: 0.5px solid #ced4da;
-	border-radius: 20px;
-	font-family: SFProDisplay;
-	padding: 20px;
-	display: flex;
-	flex-direction: column;
+  width: 500px;
+  height: auto;
+  margin: 6px;
+  box-sizing: border-box;
+  background-color: #f8f9fa;
+  color: #212529;
+  border: 0.5px solid #ced4da;
+  border-radius: 20px;
+  font-family: SFProDisplay;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const CardCommentId = styled.div`
-	display: flex;
+  display: flex;
 `;
 const CardEmail = styled.div`
-	display: flex;
-	padding-top: 10px;
+  display: flex;
+  padding-top: 10px;
 `;
 const CardComment = styled.div`
-	display: flex;
-	padding-top: 10px;
-	flex-direction: column;
+  display: flex;
+  padding-top: 10px;
+  flex-direction: column;
 `;
 const Title = styled.div`
-	font-size: 18px;
-	line-height: 21px;
-	font-weight: bold;
-	margin-right: 20px;
+  font-size: 18px;
+  line-height: 21px;
+  font-weight: bold;
+  margin-right: 12px;
 `;
 const Contents = styled.div`
-	font-size: 18px;
-	line-height: 21px;
+  font-size: 18px;
+  line-height: 21px;
 `;
