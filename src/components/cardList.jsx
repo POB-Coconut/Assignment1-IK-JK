8b1411e3d3a2e8e9bab7d5230dbe04aa5ee53c30
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-07-27 22:53:17 +0900

    feat: [#6]InfiniteScroll 구성중

--- src/components/cardList.jsx
+++ src/components/cardList.jsx
@@ -2,12 +2,24 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import request from '../data/api';
 import Card from './card';
+import { useInfiniteScroll } from './infiniteScrollList';
+
 const CardList = () => {
   const [cardData, setCardData] = useState();
+  const [target, setTarget] = useState(null);
+  const page = 1;
+  const loadData = (page) => {
+    const fetchData = request(page);
+    fetchData.then((res) => setCardData(res));
+  };
+  const loadMoreData = () => {
+    page += 1;
+    const fetchData = request(page);
+    fetchData.then((res) => setCardData(res));
+  };
 
   useEffect(() => {
-    const fetchData = request();
-    fetchData.then((res) => setCardData(res));
+    loadData();
   }, []);
 
   return (
@@ -15,6 +27,7 @@ const CardList = () => {
       {cardData?.map((card) => (
         <Card key={card.id} {...{ card }} />
       ))}
+      <div ref={setTarget}></div>
     </CardListContainer>
   );
 };
